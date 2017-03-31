064e399fc15e645c2f7ab51a2a82dd0b5dcfa887
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-03-31 07:34:32 -0700

    Added in footer and styled portfolio images.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
 // sanity check
 console.log("app.js linked.");
+
+// var img1 = document.getElementById('oakleyImage');
+
+// img1.onmouseover = function () {
+//    img1.src = 'assets/imgs/stampede.jpg';
+// };
+
+// $('#oakleyImage').hover(function() {
+//   $(this).attr('src', 'assets/imgs/oakley.jpg');
+// }, function() {
+//   $(this).attr('src', 'assets/gifs/oakley.gif');
+// });
